9e38c1d763cbdc505d90e392214b126612ce2ca3
Gustavwes <31844570+Gustavwes@users.noreply.github.com>
2018-11-06 10:00:50 +0100

    Add project files.

---
+++ GitDemoProjektLektion2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemoProjektLektion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
